3bc119c8a74cf92e00e35fcb916b58614ca13634
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-12-11 13:31:28 +0100

    Create easy1Test.php

---
+++ tests/syntax/easy/easy1Test.php
@@ -0,0 +1,11 @@
+<?php
+
+class easy1Test extends PHPUnit_Framework_TestCase
+{
+    public function testVariableIsAssigned()
+    {
+        $this->assertEquals('value', $variable);
+    }
+}
+
+?>
