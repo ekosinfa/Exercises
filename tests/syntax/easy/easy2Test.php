acbc96f3864456c2c0c348cde9e5e5fe4cfc3290
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-12-11 17:31:02 +0100

    Fix numbering [ci skip]

--- tests/syntax/easy/easy2Test.php
+++ tests/syntax/easy/easy2Test.php
@@ -2,7 +2,7 @@
 
 require __DIR__.'/../../../src/syntax/easy/easy2.php';
 
-class easy1Test extends PHPUnit_Framework_TestCase
+class easy2Test extends PHPUnit_Framework_TestCase
 {
     public function testVariableIsAssigned()
     {
