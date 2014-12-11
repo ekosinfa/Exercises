f426338fe94d16dd218133f498f1e84dbc9831f4
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-12-11 15:38:55 +0100

    Update bulid.sh

--- bulid.sh
+++ bulid.sh
@@ -1,11 +1,11 @@
 #!/bin/bash  
 
 find . -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
-$RETVAL = $?
-if [$? == 0]
+
+if ['$?' == 0]
 then 
   phpunit
   return $?
 else
-  return $RETVAL
+  return 1
 fi
