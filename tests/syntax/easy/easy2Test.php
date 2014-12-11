56b7825332f096db119a1218495473093ac9ff09
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-12-11 16:37:54 +0100

    [ci skip]

---
+++ tests/syntax/easy/easy2Test.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__.'/../../../src/syntax/easy/easy2.php';
+
+class easy1Test extends PHPUnit_Framework_TestCase
+{
+    public function testVariableIsAssigned()
+    {
+        $this->assertEquals('value', returnNumbers()());
+    }
+}
+
+?>
