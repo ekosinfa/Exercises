6430b81e08480cb0a4fb71b31639ce70424ecd2a
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-12-11 16:06:26 +0100

    Update bulid.sh

--- bulid.sh
+++ bulid.sh
@@ -1,11 +1,7 @@
 #!/bin/bash  
 
-find . -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
-
-if ['$?' == 0]
+if find . -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
 then 
   phpunit
   return $?
-else
-  return 1
 fi
